01529add0606c4cc51011bcdb689b642e81ae51a
KhVitaly <117944757+KhVitaly@users.noreply.github.com>
2024-03-21 20:33:06 +0300

    laba_ver2

--- 2_laba_ver1.cpp
+++ 2_laba_ver1.cpp
@@ -5,7 +5,10 @@ using namespace std;
 
 
 // Функция, которую мы будем интегрировать
-double function(double);
+double calculated_function(double);
+
+//Вычисление шага интегрирования
+double step_calculator(double,double,int);
 
 // Метод левых прямоугольников для численного интегрирования
 double left_rect_method(double, double, int);
@@ -16,14 +19,10 @@ double right_rect_method(double, double, int);
 // Метод трапеций для численного интегрирования
 double trapez_method(double, double, int);
 
-// Метод левых прямоугольников для численного интегрирования с заданной точностью
-double left_rect_method_precision(double, double, int, int, double);
+// Метод вычисления интеграла с заданной точностью и начальным шагом
+void integration_precision(double integral_method(double, double, int), double, double, int, int, double, double);
 
-// Метод правых прямоугольников для численного интегрирования с заданной точностью
-double right_rect_method_precision(double, double, int, int, double);
 
-// Метод трапеций для численного интегрирования с заданной точностью
-double trapez_method_precision(double, double, int, int, double);
 
 
 int main()
@@ -67,36 +66,47 @@ int main()
                 cin >> qty_segments;
 
 
-                double Integral_left_rect_method = left_rect_method(x_min, x_max, qty_segments); // метод левых прямоугольников
-                cout << "\tIntegral_left_rect_method = " << Integral_left_rect_method << endl;;
+                // метод левых прямоугольников
+                cout << "\tIntegral_left_rect_method = " << left_rect_method(x_min, x_max, qty_segments) << endl;;
 
-                double Integral_right_rect_method = right_rect_method(x_min, x_max, qty_segments); // метод правых прямоугольников
-                cout << "\tIntegral_right_rect_method = " << Integral_right_rect_method << endl;
+                // метод правых прямоугольников
+                cout << "\tIntegral_right_rect_method = " << right_rect_method(x_min, x_max, qty_segments) << endl;
 
-                double Integral_trapez_method = trapez_method(x_min, x_max, qty_segments); // метод трапеций
-                cout << "\tIntegral_trapez_method = " << Integral_trapez_method << endl;
+                // метод трапеций
+                cout << "\tIntegral_trapez_method = " << trapez_method(x_min, x_max, qty_segments) << endl;
 
                 break;
             }
 
+
             case 2: // Calculate integral WITH precision
             {
                 double precision{}; // точность
-                int start_qty_segments{}; // начальное значение кол-ва шагов
+                int qty_segments, qty_segments_max = 1000000;
+                double x_min, x_max, mult;
 
-                cout << "Input precision(E) = ";
-                cin >> precision;                
-                cout << "Input start_qty_segments = ";
-                cin >> start_qty_segments;
+                cout << "Input x_min = ";
+                cin >> x_min;
+                cout << "Input x_max = ";
+                cin >> x_max;
+                cout << "Input qty_segments = ";
+                cin >> qty_segments;
+                cout << "Enter multiplier = ";
+                cin >> mult;
+                cout << "Enter precision = ";
+                cin >> precision;
 
-                double Integral_left_rect_method_precision = left_rect_method_precision(x_min, x_max, start_qty_segments, qty_segments, precision); // метод левых прямоугольников
-                cout << "\tIntegral_left_rect_method_precision = " << Integral_left_rect_method_precision << endl;
+                // метод левых прямоугольников
+                cout << "\tIntegral_left_rect_method_precision(e) -> ";
+                integration_precision(left_rect_method, x_min, x_max, qty_segments, qty_segments_max, mult, precision);
                 
-                double Integral_right_rect_method_precision = right_rect_method_precision(x_min, x_max, start_qty_segments, qty_segments, precision); // метод правых прямоугольников
-                cout << "\tIntegral_right_rect_method_precision = " << Integral_right_rect_method_precision << endl;
+                // метод правых прямоугольников
+                cout << "\tIntegral_right_rect_method_precision(e) -> ";
+                integration_precision(right_rect_method, x_min, x_max, qty_segments, qty_segments_max, mult, precision);
 
-                double Integral_trapez_method_precision = trapez_method_precision(x_min, x_max, start_qty_segments, qty_segments, precision); // метод тарпеций 
-                cout << "\tIntegral_trapez_method_precision = " << Integral_trapez_method_precision << endl;
+                // метод тарпеций 
+                cout << "\tIntegral_trapez_method_precision(e) -> ";
+                integration_precision(trapez_method, x_min, x_max, qty_segments, qty_segments_max, mult, precision);
 
                 break;
             }
@@ -117,29 +127,38 @@ int main()
 
 
 // Функция, которую мы будем интегрировать
-double function(double x) 
+double calculated_function(double x) 
 {
     return x/8 + sin(x * x);
     //return (cos(0.8*x+1.2))/(1/5+sin(x*x+0.6));
 }
 
+//Вычисление шага интегрирования
+double step_calculator(double x_min,double x_max, int qty_segments){
+    double step;
+    step = (x_max - x_min) / qty_segments;
+
+    return step;
+}
+
+
 // Метод левых прямоугольников для численного интегрирования
 double left_rect_method(double x_min, double x_max, int qty_segments) 
 {
-    double step = (x_max - x_min) / qty_segments; // Шаг разбиения интервала интегрирования
+    double x_left = x_min;
+    double step = step_calculator(x_min, x_max, qty_segments);
     double integral{};
 
     // Цикл для вычисления суммы значений функции на каждом прямоугольнике
-    for (int i = 0; i <= qty_segments - 1; i++) 
+    for (int i = 0; i < qty_segments - 1; i++) 
     {
-        integral += function(x_min + step * i) * step; // Площадь i-го прямоугольника идет в сумму
+        integral += fabs(calculated_function(x_left) * step); // Площадь i-го прямоугольника идет в сумму
+        x_left += step;
     }
 
     // Вычисление последней(остаточной) площади отдельно
     // Левую границу последнего прямоугольника умножаем на размер последнего шага
-    double x_left_last = x_min + (qty_segments - 1) * step;
-    double last_step = x_max - x_min - (qty_segments - 1) * step;
-    integral += function(x_left_last) * last_step;
+    integral += fabs(calculated_function(x_left) * (x_max - x_left));
 
     return integral;
 }
@@ -148,19 +167,19 @@ double left_rect_method(double x_min, double x_max, int qty_segments)
 // Метод правых прямоугольников для численного интегрирования
 double right_rect_method(double x_min, double x_max, int qty_segments) 
 {
-    double step = (x_max - x_min) / qty_segments; // Шаг интегрирования
+    double x_right = x_max;
+    double step = step_calculator(x_min, x_max, qty_segments);
     double integral{};
 
     
-    for (int i = 1; i <= qty_segments; i++)
+    for (int i = 0; i < qty_segments - 1; i++)
     {
-        integral += function(x_min + step * i) * step; // Суммируем значения функции в точках правых краев прямоугольников
+        integral += fabs(calculated_function(x_right) * step); // Суммируем значения функции в точках правых краев прямоугольников
+        x_right -= step;
     }
 
     // Вычисление последней(остаточной) площади отдельно
-    double x_right_last = x_max;
-    double last_step = x_max - x_min - (qty_segments - 1) * step;
-    integral += function(x_right_last) * last_step;
+    integral += fabs(calculated_function(x_right) * (x_right - x_min));
 
     return integral;
 }
@@ -169,74 +188,42 @@ double right_rect_method(double x_min, double x_max, int qty_segments)
 // Метод трапеций для численного интегрирования
 double trapez_method(double x_min, double x_max, int qty_segments) 
 {
-    double step = (x_max - x_min) / qty_segments; // Вычисляем шаг
-    double integral = function(x_min) + function(x_max); // Суммируем краевые точки
+    double step = step_calculator(x_min, x_max, qty_segments);
+    double x_left = x_min;
+    double x_next = x_min + step;
+    double integral{};
 
     // Суммируем значения внутри интервала с использованием трапеций
-    for (int i = 1; i <= qty_segments - 1; i++)
+    for (int i = 0; i < qty_segments - 1; i++)
     {
-        integral += 2 * function(x_min + step * i);
+        integral += fabs((calculated_function(x_left) + calculated_function(x_next)) / 2 * step);
+        x_left = x_next;
+        x_next += step;
     }
 
-    integral *= step/2;
-
-    // Вычисление последней(остаточной) площади отдельно        ??????
-    double x_left_last = x_min + (qty_segments - 1) * step;
-    double x_right_last = x_max;
-    double last_step = x_max - x_min - (qty_segments - 1) * step;
-    integral += (function(x_left_last) + function(x_right_last)) * last_step / 2;
-    //integral += ((function(x_min + (qty_segments - 1) * step) + function(x_max)) / 2) * step;
+    // Вычисление последней(остаточной) площади отдельно 
+    integral += fabs((calculated_function(x_left) + calculated_function(x_max)) / 2 * (x_max - x_left));
 
     return integral;
 }
 
+// Метод вычисления интеграла с заданной точностью и начальным шагом
+void integration_precision(double integral_method(double, double, int), double x_min, double x_max, int qty_segments, int qty_segments_max, double mult, double precision)
+{
+    double I1, I2;//, precision;
 
-// Метод левых прямоугольников для численного интегрирования с заданной точностью
-double left_rect_method_precision(double x_min, double x_max, int start_qty_segments, int qty_segments, double precision) 
-{   
-    double I1, I2;
-    I2 = left_rect_method(x_min, x_max, start_qty_segments);
-    do
+    while (fabs(I1 - I2) >= precision || qty_segments < qty_segments_max)
     {
         I1 = I2;
-        start_qty_segments *= 2;
-        I2 = left_rect_method(x_min, x_max, start_qty_segments);
+        qty_segments *= mult; // увеличиваем кол-во шагов
+        I2 = integral_method(x_min, x_max, qty_segments);
+
+
+        if (fabs(I1 - I2) < precision && qty_segments < qty_segments_max)
+        {
+           cout << " " << I2 << "\n";
+           return;
+        }
     }
-    while (fabs(I1 - I2) <= precision || start_qty_segments > qty_segments);
 
-    return I2;
-}
-
-
-// Метод правых прямоугольников для численного интегрирования с заданной точностью
-double right_rect_method_precision(double x_min, double x_max, int start_qty_segments, int qty_segments, double precision) 
-{   
-    double I1, I2;
-    I2 = right_rect_method(x_min, x_max, start_qty_segments);
-    do
-    {
-        I1 = I2;
-        start_qty_segments *= 2;
-        I2 = right_rect_method(x_min, x_max, start_qty_segments);
-    }
-    while (fabs(I1 - I2) <= precision || start_qty_segments > qty_segments);
-
-    return I2;
-}
-
-
-// Метод трапеций для численного интегрирования с заданной точностью
-double trapez_method_precision(double x_min, double x_max, int start_qty_segments, int qty_segments, double precision) 
-{   
-    double I1, I2;
-    I2 = trapez_method(x_min, x_max, start_qty_segments);
-    do
-    {
-        I1 = I2;
-        start_qty_segments *= 2;
-        I2 = trapez_method(x_min, x_max, start_qty_segments);
-    }
-    while (fabs(I1 - I2) <= precision || start_qty_segments > qty_segments);
-
-    return I2;
 }
